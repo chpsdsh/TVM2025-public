76b451efa22e2e631c11c02b81d1e7d8dabe1c9d
chpsdsh.fedora <bmxsib1@gmail.com>
2025-09-14 23:30:20 +0700

    lab01 added functions

--- lab01/src/calculate.ts
+++ lab01/src/calculate.ts
@@ -5,20 +5,35 @@ export const addMulSemantics: AddMulSemantics = grammar.createSemantics() as Add
 
 
 const addMulCalc = {
-    AddExp_plus(arg0: any, _plus: any, arg1: any) {
-        return arg0.calculate() + arg1.calculate();
+    AddExp_plus(arg0: any, arg1: any, arg2: any) {
+        return arg0.calculate() + arg2.calculate();
     },
 
-    MulExp_mul(arg0: any, _times: any, arg1: any) {
-        return arg0.calculate() * arg1.calculate();
+    AddExp(arg0: any) {
+        return arg0.calculate();
+    },
+
+    MulExp_mul(arg0: any, arg1: any, arg2: any) {
+        return arg0.calculate() * arg2.calculate();
+    },
+
+    MulExp(arg0: any) {
+        return arg0.calculate();
     },
 
     PriExp_paren(arg0: any, arg1: any, arg2: any) {
         return arg1.calculate();
     },
 
+    PriExp(arg0: any) {
+        return arg0.calculate();
+    },
+
     number_whole(arg0: any) {
         return parseInt(this.sourceString, 10);
+    },
+    number(arg0: any) {
+        return arg0.calculate();
     }
 } satisfies AddMulActionDict<number>
 
