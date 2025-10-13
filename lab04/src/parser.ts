5f986f3bdcc247d7056f1b88015a2c3f17a460ac
chpsdsh.fedora <bmxsib1@gmail.com>
2025-10-13 09:30:00 +0700

    done

--- lab04/src/parser.ts
+++ lab04/src/parser.ts
@@ -3,72 +3,71 @@ import { arithGrammar, ArithmeticActionDict, ArithmeticSemantics, SyntaxError } 
 import { Expr } from './ast';
 
 export const getExprAst: ArithmeticActionDict<Expr> = {
-  Expr(this: any, add: any): Expr {
-    return add.parse();
+  Expr(this: any, addExp: any): Expr {
+    return addExp.parse();
   },
 
-AddExp(this: any, first: any, opsIter: any, termsIter: any): Expr {
-  let node: Expr = first.parse();
+  AddExp(this: any, first: any, ops: any, terms: any): Expr {
+    let node: Expr = first.parse();
 
-  const ops = opsIter.children;     
-  const terms = termsIter.children; 
+    const opNodes = ops.children;    
+    const termNodes = terms.children; 
 
-  for (let i = 0; i < ops.length; i++) {
-    const op = ops[i].sourceString;      
-    const rhs: Expr = terms[i].parse();  
-    node = op === "+"
-      ? { kind: "Add", left: node, right: rhs }
-      : { kind: "Sub", left: node, right: rhs };
-  }
-  return node;
-},
-
-MulExp(this: any, first: any, opsIter: any, termsIter: any): Expr {
-  let node: Expr = first.parse();
-
-  const ops = opsIter.children;     
-  const terms = termsIter.children; 
-
-  for (let i = 0; i < ops.length; i++) {
-    const op = ops[i].sourceString;      
-    const rhs: Expr = terms[i].parse();  
-    node = op === "*"
-      ? { kind: "Mul", left: node, right: rhs }
-      : { kind: "Div", left: node, right: rhs };
-  }
-  return node;
-},
-
-
-  Unary_neg(this: any, _minus: any, u: any): Expr {
-    return { kind: "Neg", expr: u.parse() };
+    for (let i = 0; i < opNodes.length; i++) {
+      const op = opNodes[i].sourceString;         
+      const rhs: Expr = termNodes[i].parse();     
+      node = op === "+"
+        ? { kind: "Add", left: node, right: rhs }
+        : { kind: "Sub", left: node, right: rhs };
+    }
+    return node;
   },
 
-  
-  Unary_prim(this: any, p: any): Expr {
-    return p.parse();
+  MulExp(this: any, first: any, ops: any, terms: any): Expr {
+    let node: Expr = first.parse();
+
+    const opNodes = ops.children;     
+    const termNodes = terms.children; 
+
+    for (let i = 0; i < opNodes.length; i++) {
+      const op = opNodes[i].sourceString;        
+      const rhs: Expr = termNodes[i].parse();    
+      node = op === "*"
+        ? { kind: "Mul", left: node, right: rhs }
+        : { kind: "Div", left: node, right: rhs };
+    }
+    return node;
   },
 
-  Unary(this: any, n: any): Expr {
-    return n.parse();
+  Unary_neg(this: any, minusTok: any, unary: any): Expr {
+    return { kind: "Neg", expr: unary.parse() };
   },
 
-  PriExp_paren(this: any, _l: any, e: any, _r: any): Expr {
-    return e.parse();
+  Unary_prim(this: any, priExp: any): Expr {
+    return priExp.parse();
   },
 
-  PriExp(this: any, n: any): Expr {
-    return n.parse();
+  Unary(this: any, unary: any): Expr {
+    return unary.parse();
   },
 
-  variable(this: any, _h: any, _t: any): Expr {
+  PriExp_paren(this: any, lparen: any, addExp: any, rparen: any): Expr {
+    return addExp.parse();
+  },
+
+  PriExp(this: any, prim: any): Expr {
+    return prim.parse();
+  },
+
+  variable(this: any, head: any, tail: any): Expr {
     return { kind: "Var", name: this.sourceString };
   },
 
-  number(this: any, _ds: any): Expr {
+  number(this: any, digits: any): Expr {
     return { kind: "Num", value: Number(this.sourceString) };
   },
 };
+
 
 export const semantics = arithGrammar.createSemantics();
 
